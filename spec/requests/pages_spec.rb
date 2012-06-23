3259e5a7d0b1a78c7e1135abc6debaeaebe02f0d
David Tengdin <dtengdin@m2s.com>
2012-06-23 00:17:20 -0400

    fixed gemfile to properly call bootstrap, app works

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+    describe "Pages", :type => :request do
+      describe "GET /" do
+        it "is on the home page" do
+          visit '/'
+          current_path.should == '/'
+        end
+      end
+      describe "GET /about" do
+        it "is on the About page" do
+          visit '/about'
+          current_path.should == '/about'
+        end
+      end
+    end
