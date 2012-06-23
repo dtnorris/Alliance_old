11cfb13530a7e2657fc4729cec06d6a2cf9bf437
David Tengdin <dtengdin@m2s.com>
2012-06-23 10:22:39 -0400

    Revert "Revert "fixed gemfile to properly call bootstrap, app works""
    This reverts commit b6e5e727a84c4614070a4deaa5f418e4b29eff98.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -97,7 +97,9 @@ SimpleForm.setup do |config|
   # Default class for buttons
   config.button_class = 'btn'
 
-  # Method used to tidy up errors.
+  # Method used to tidy up errors. Specify any Rails Array method.
+  # :first lists the first message for each field.
+  # Use :to_sentence to list all errors for each field.
   # config.error_method = :first
 
   # Default tag used for error notification helper.
@@ -137,7 +139,6 @@ SimpleForm.setup do |config|
 
   # You can define the class to use on all forms. Default is simple_form.
   # config.form_class = :simple_form
-  config.form_class = 'simple_form form-horizontal'
 
   # You can define which elements should obtain additional classes
   # config.generate_additional_classes_for = [:wrapper, :label, :input]
