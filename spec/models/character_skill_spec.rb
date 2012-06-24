72590789f8ca615b30fae8a17e795ed735809143
David Tengdin <dtengdin@m2s.com>
2012-06-23 20:45:01 -0400

    added test for attribute filtering method in CharacterSkill

--- spec/models/character_skill_spec.rb
+++ spec/models/character_skill_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
-# describe CharacterSkill do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe CharacterSkill do
+
+  it "can filter database columns not to display" do
+    char = CharacterSkill.create
+    arr = [true]
+    char.attributes.each {|atn, atv| arr << CharacterSkill.display_val(atn)}
+    arr.map {|e| e if !e}.compact.count.should == 3
+  end
+
+end
