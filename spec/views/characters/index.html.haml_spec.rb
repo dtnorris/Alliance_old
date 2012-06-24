f69b4865fdc7fe4118855c7a418e7a0cc6df2ba7
David Tengdin <dtengdin@m2s.com>
2012-06-24 15:43:12 -0400

    fixed broken spec for character_class

--- spec/views/characters/index.html.haml_spec.rb
+++ spec/views/characters/index.html.haml_spec.rb
@@ -7,13 +7,15 @@ describe "characters/index" do
         :name => "Name",
         :build_points => 1,
         :experience_points => 2,
-        :race_id => 11
+        :race_id => 11,
+        :character_class_id => 1
       ),
       stub_model(Character,
         :name => "Name",
         :build_points => 1,
         :experience_points => 2,
-        :race_id => 11
+        :race_id => 11,
+        :character_class_id => 1
       )
     ])
   end
@@ -24,5 +26,6 @@ describe "characters/index" do
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => Race.find_by_id(11).name.to_s, :count => 2
+    assert_select "tr>td", :text => CharacterClass.find_by_id(1).name.to_s, :count => 2
   end
 end
